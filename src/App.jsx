b6335344ac1f781097e6147c8ff9c39a3a536629
Rohyer <guilhermerl.dev@gmail.com>
2023-11-09 22:28:19 -0300

    Add UserContext to another components

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-
-        <Footer />
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
