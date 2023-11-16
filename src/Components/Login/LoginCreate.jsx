de963ade3297d4d1eb628e22ae5a3df9c6125be8
Rohyer <guilhermerl.dev@gmail.com>
2023-11-15 23:34:57 -0300

    Add useFetch to LoginCreate component

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -4,40 +4,49 @@ import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import { USER_POST } from '../../Api';
 import { UserContext } from '../../UserContext';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 
 const LoginCreate = () => {
-
   const username = useForm();
   const email = useForm('email');
   const password = useForm();
 
-  const {userLogin} = React.useContext(UserContext);
+  const { userLogin } = React.useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const {url, options} = USER_POST({
+    const { url, options } = USER_POST({
       username: username.value,
       email: email.value,
       password: password.value,
     });
-    
-    const response = await fetch(url, options);
+
+    const { response } = await request(url, options);
 
     if (response.ok) userLogin(username.value, password.value);
   }
 
-  return <section className='animeLeft'>
-    <h1 className='title'>Cadastre-se</h1>
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
 
-    <form onSubmit={handleSubmit}>
-      <Input label="Usuário" type="text" name="username" {...username} />
-      <Input label="Email" type="email" name="email" {...email} />
-      <Input label="Senha" type="password" name="password" {...password} />
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
 
-      <Button>Cadastrar</Button>
-    </form>
-  </section>;
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
