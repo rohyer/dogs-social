0b926862d465a9350e4be36eb1a9fcb176a010c7
Rohyer <guilhermerl.dev@gmail.com>
2023-11-23 21:18:19 -0300

    Import NotFound to create its routes

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -5,9 +5,10 @@ import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 import { UserContext } from '../../UserContext';
+import NotFound from '../NotFound';
 
 const User = () => {
-  const {data} = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <section className="container">
@@ -17,6 +18,7 @@ const User = () => {
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
