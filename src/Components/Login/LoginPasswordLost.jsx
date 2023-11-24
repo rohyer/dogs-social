0d0304d911af45acb601aec9ccdca5292931c891
Rohyer <guilhermerl.dev@gmail.com>
2023-11-24 12:09:05 -0300

    Develop LoginPasswordLost component

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const email = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    request(url, options);
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="E-mail / Usuário" type="text" name="email" {...email} />
+        {email.value}
+        <Button>Envia E-mail</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
