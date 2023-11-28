de18d8deec180bdc4fec9b60747539c1a716e465
Rohyer <guilhermerl.dev@gmail.com>
2023-11-28 07:42:34 -0300

    Import Head to Photo component

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -5,6 +5,7 @@ import { PHOTO_GETT } from '../../Api';
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import PhotoContent from './PhotoContent';
+import Head from '../Helper/Head';
 
 const Photo = () => {
   const { id } = useParams();
@@ -21,6 +22,7 @@ const Photo = () => {
   if (data)
     return (
       <section className="container mainContainer">
+        <Head title={data.photo.title} />
         <PhotoContent single={true} data={data} />
       </section>
     );
