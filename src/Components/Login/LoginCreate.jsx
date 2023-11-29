9aa6522eb7d7e51be4141cd941ee97af0d6165ab
Rohyer <guilhermerl.dev@gmail.com>
2023-11-29 07:35:07 -0300

    Import Head to LoginCreate component

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -6,6 +6,7 @@ import { USER_POST } from '../../Api';
 import { UserContext } from '../../UserContext';
 import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
+import Head from '../Helper/Head';
 
 const LoginCreate = () => {
   const username = useForm();
@@ -31,6 +32,7 @@ const LoginCreate = () => {
 
   return (
     <section className="animeLeft">
+      <Head title="Crie sua conta" />
       <h1 className="title">Cadastre-se</h1>
 
       <form onSubmit={handleSubmit}>
